c4a8d036e6404a937868360b0bc66d9ac7ade8e1
Jake <jake.ryan@digipen.edu>
2020-10-11 03:14:21 -0700

    Tabs -> spaces.

--- Water/Source/resources/Shaders/heightWater.fs
+++ Water/Source/resources/Shaders/heightWater.fs
@@ -18,43 +18,43 @@ uniform samplerCube envTex;
 
 float FresnelSchlick(vec3 i, vec3 n, float Eta, float Power)
 {
-	float F = ((1.0-Eta) * (1.0-Eta)) / ((1.0+Eta) * (1.0+Eta));
-	return F + (1.0 - F) * pow((1.0 - dot(-i, n)), Power);
+    float F = ((1.0-Eta) * (1.0-Eta)) / ((1.0+Eta) * (1.0+Eta));
+    return F + (1.0 - F) * pow((1.0 - dot(-i, n)), Power);
 }
 
 
 vec3 CalcEnvColor(vec3 normal, vec3 viewDir)
 {
-	float inDex = u_waterRefract; 
-	float exDex = u_airRefract;
+    float inDex = u_waterRefract; 
+    float exDex = u_airRefract;
 
-	// viewing from under water
-	if (dot(normal, -viewDir) > 0)
-	{
-		float tmp = inDex;
-		inDex = exDex;
-		exDex = tmp;
-		normal *= -1;
-	}
-	float eta = exDex / inDex;
+    // viewing from under water
+    if (dot(normal, -viewDir) > 0)
+    {
+        float tmp = inDex;
+        inDex = exDex;
+        exDex = tmp;
+        normal *= -1;
+    }
+    float eta = exDex / inDex;
     float Ratio = FresnelSchlick(-viewDir, normal, eta, FRESNEL_POWER);
 
-	vec3 reflectColor = texture(envTex, reflect(-viewDir, normal)).rgb;
-	vec3 refractColor = texture(envTex, refract(-viewDir, normal, eta)).rgb;
-	
-	// snell's window
-	if (all(lessThan(abs(refract(-viewDir, normal, eta)), vec3(0.1))))
-		refractColor = reflectColor;
-	return mix(refractColor, reflectColor, Ratio);
+    vec3 reflectColor = texture(envTex, reflect(-viewDir, normal)).rgb;
+    vec3 refractColor = texture(envTex, refract(-viewDir, normal, eta)).rgb;
+    
+    // snell's window
+    if (all(lessThan(abs(refract(-viewDir, normal, eta)), vec3(0.1))))
+        refractColor = reflectColor;
+    return mix(refractColor, reflectColor, Ratio);
 }
 
 
 void main()
 {
-	vec3 viewDir = normalize(u_viewpos - vPos);
-	vec3 normal = normalize(vNormal);
+    vec3 viewDir = normalize(u_viewpos - vPos);
+    vec3 normal = normalize(vNormal);
 
-	vec3 envColor = CalcEnvColor(normal, viewDir);
+    vec3 envColor = CalcEnvColor(normal, viewDir);
 
-	FragColor = vec4(envColor, 1.0);
+    FragColor = vec4(envColor, 1.0);
 }
